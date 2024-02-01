8e86d2d2995f1f49e07bdc4ac99ef673b78f19f9
Che Danial Afnan <chedanialafnan28@gmail.com>
2024-02-01 12:45:33 +0800

    Update get_data_from_public_api_to_mysql .py

--- api-server/src/get_data_from_public_api_to_mysql .py
+++ api-server/src/get_data_from_public_api_to_mysql .py
@@ -24,8 +24,63 @@ def fetch_and_update():
             cursor = db_connection.cursor()
 
             for item in api_data:
-                query = "INSERT INTO national_state (station_id, station_name, latitude, longitude, district, state, sub_basin, main_basin, station_type, water_level_current, water_level_indicator, water_level_normal_level, water_level_alert_level, water_level_warning_level, water_level_danger_level, water_level_increment, water_level_update_datetime, water_level_update_date, water_level_trend, rainfall_clean, rainfall_latest_1hr, rainfall_total_today, rainfall_indicator, rainfall_update_datetime, rainfall_update_date, water_level_display, rainfall_display, raw_water_level, raw_rainfall, station_status, station_code, water_level_status, rainfall_status) VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s)"
-                values = (item['station_id'], item['station_name'], item['latitude'], item['longitude'], item['district'], item['state'], item['sub_basin'], item['main_basin'], item['station_type'], item['water_level_current'], item['water_level_indicator'], item['water_level_normal_level'], item['water_level_alert_level'], item['water_level_warning_level'], item['water_level_danger_level'], item['water_level_increment'], item['water_level_update_datetime'], item['water_level_update_date'], item['water_level_trend'], item['rainfall_clean'], item['rainfall_latest_1hr'], item['rainfall_total_today'], item['rainfall_indicator'], item['rainfall_update_datetime'], item['rainfall_update_date'], item['water_level_display'], item['rainfall_display'], item['raw_water_level'], item['raw_rainfall'], item['station_status'], item['station_code'], item['water_level_status'], item['rainfall_status'])
+                query = """
+                    INSERT INTO national_state 
+                    (station_id, station_name, latitude, longitude, district, state, sub_basin, main_basin, station_type, 
+                    water_level_current, water_level_indicator, water_level_normal_level, water_level_alert_level, 
+                    water_level_warning_level, water_level_danger_level, water_level_increment, water_level_update_datetime, 
+                    water_level_update_date, water_level_trend, rainfall_clean, rainfall_latest_1hr, rainfall_total_today, 
+                    rainfall_indicator, rainfall_update_datetime, rainfall_update_date, water_level_display, rainfall_display, 
+                    raw_water_level, raw_rainfall, station_status, station_code, water_level_status, rainfall_status) 
+                    VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s, %s, %s,%s, %s, %s)
+                    ON DUPLICATE KEY UPDATE
+                    station_name=VALUES(station_name),
+                    latitude=VALUES(latitude),
+                    longitude=VALUES(longitude),
+                    district=VALUES(district),
+                    state=VALUES(state),
+                    sub_basin=VALUES(sub_basin),
+                    main_basin=VALUES(main_basin),
+                    station_type=VALUES(station_type),
+                    water_level_current=VALUES(water_level_current),
+                    water_level_indicator=VALUES(water_level_indicator),
+                    water_level_normal_level=VALUES(water_level_normal_level),
+                    water_level_alert_level=VALUES(water_level_alert_level),
+                    water_level_warning_level=VALUES(water_level_warning_level),
+                    water_level_danger_level=VALUES(water_level_danger_level),
+                    water_level_increment=VALUES(water_level_increment),
+                    water_level_update_datetime=VALUES(water_level_update_datetime),
+                    water_level_update_date=VALUES(water_level_update_date),
+                    water_level_trend=VALUES(water_level_trend),
+                    rainfall_clean=VALUES(rainfall_clean),
+                    rainfall_latest_1hr=VALUES(rainfall_latest_1hr),
+                    rainfall_total_today=VALUES(rainfall_total_today),
+                    rainfall_indicator=VALUES(rainfall_indicator),
+                    rainfall_update_datetime=VALUES(rainfall_update_datetime),
+                    rainfall_update_date=VALUES(rainfall_update_date),
+                    water_level_display=VALUES(water_level_display),
+                    rainfall_display=VALUES(rainfall_display),
+                    raw_water_level=VALUES(raw_water_level),
+                    raw_rainfall=VALUES(raw_rainfall),
+                    station_status=VALUES(station_status),
+                    station_code=VALUES(station_code),
+                    water_level_status=VALUES(water_level_status),
+                    rainfall_status=VALUES(rainfall_status);
+                """
+                values = (
+                    item['station_id'], item['station_name'], item['latitude'], item['longitude'],
+                    item['district'], item['state'], item['sub_basin'], item['main_basin'],
+                    item['station_type'], item['water_level_current'], item['water_level_indicator'],
+                    item['water_level_normal_level'], item['water_level_alert_level'],
+                    item['water_level_warning_level'], item['water_level_danger_level'],
+                    item['water_level_increment'], item['water_level_update_datetime'],
+                    item['water_level_update_date'], item['water_level_trend'], item['rainfall_clean'],
+                    item['rainfall_latest_1hr'], item['rainfall_total_today'], item['rainfall_indicator'],
+                    item['rainfall_update_datetime'], item['rainfall_update_date'],
+                    item['water_level_display'], item['rainfall_display'], item['raw_water_level'],
+                    item['raw_rainfall'], item['station_status'], item['station_code'],
+                    item['water_level_status'], item['rainfall_status']
+                )
                 cursor.execute(query, values)
 
             db_connection.commit()
@@ -41,7 +96,7 @@ def fetch_and_update():
         print(f"Error: {e}")
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(fetch_and_update, 'interval', seconds=30)
+scheduler.add_job(fetch_and_update, 'interval', seconds=30, max_instances=10)
 scheduler.start()
 
 if __name__ == "__main__":
